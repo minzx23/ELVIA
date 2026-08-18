810c604865d2bd3ef65fae46359a73ee6159ee94
minzx23 <kmjn0623@gmail.com>
2026-08-18 19:50:15 +0900

    ✨Feat: 자세히보기 버튼 UI 구현

---
+++ src/components/ui/ShowButton.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ShowButton() {
+  return (
+    <button className="h-9.5 w-25 rounded-md bg-[#16305C] px-4 py-2.25 text-[13px] font-semibold text-white hover:cursor-pointer hover:bg-[#0F1E3D]">
+      자세히보기
+    </button>
+  );
+}
