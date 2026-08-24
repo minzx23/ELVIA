5a35b8d46747ced71b7dfd88714b5b7ecbd0cd75
minzx23 <kmjn0623@gmail.com>
2026-08-24 16:09:40 +0900

    💄Design: 자세히보기 버튼 반응형 UI 수정

--- src/components/ui/ShowButton.tsx
+++ src/components/ui/ShowButton.tsx
@@ -2,7 +2,7 @@
 
 export default function ShowButton() {
   return (
-    <button className="h-9.5 w-25 rounded-md bg-[#16305C] px-4 py-2.25 text-[13px] font-semibold text-white hover:cursor-pointer hover:bg-[#0F1E3D]">
+    <button className='h-9.5 w-25 rounded-md bg-[#16305C] px-4 py-2.25 text-[13px] font-semibold text-white hover:cursor-pointer hover:bg-[#0F1E3D] md:h-8 md:w-24 md:px-3 md:py-2 md:text-[11px] lg:h-7 lg:w-18.75 lg:px-2 lg:py-2 lg:text-[9px]'>
       자세히보기
     </button>
   );
