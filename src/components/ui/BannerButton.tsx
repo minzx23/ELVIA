2a667cd5596f6b8408dda5b8ef64f932084b9eb4
minzx23 <kmjn0623@gmail.com>
2026-08-24 17:26:09 +0900

    🔥Remove: 제품 보기 버튼 삭제

--- src/components/ui/BannerButton.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function BannerButton() {
-  return (
-    <button className="h-12.5 w-31 rounded-md bg-[#4E7CD4] text-[15px] font-bold text-white hover:cursor-pointer hover:bg-[#3a5bb0]">
-      제품 보기
-    </button>
-  );
-}
