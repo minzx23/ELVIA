5e87b7c0d266a264766a5248ed4191db731e9d04
minzx23 <kmjn0623@gmail.com>
2026-09-01 16:56:51 +0900

    ✨Feat: 헤더 로고 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link';
 import HeaderMenu from './HeaderMenu';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 flex items-center justify-between bg-[#0E2447] px-8 py-4'>
-      <Link href='/' className='flex items-center gap-2'>
-        <span>로고</span>
+    <header className='sticky top-0 z-50 flex items-center justify-between bg-[#0E2447]'>
+      <Link href='/' className='flex items-center gap-2 text-white'>
+        <span className='px-8'>
+          <Image src='/DNT_logo_white.png' alt='logo' width={120} height={120} />
+        </span>
       </Link>
 
-      <HeaderMenu />
+      <div className='px-8 py-4'>
+        <HeaderMenu />
+      </div>
     </header>
   );
 }
