aa70d01e76229257e78c0cbe1bcb115bacfe5cf1
minzx23 <kmjn0623@gmail.com>
2026-09-07 16:11:23 +0900

    📦️Chore: 레이아웃에 푸터 추가 및 비율 조정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import Header from '@/components/header/Header';
 import './globals.css';
 import type { ReactNode } from 'react';
+import Footer from '@/components/footer/Footer';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
-      <body>
+      <body className='flex min-h-screen flex-col'>
         <Header />
-        {children}
+        <main className='flex-1'>{children}</main>
+        <Footer />
       </body>
     </html>
   );
