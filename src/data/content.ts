5f3b3a911f2230df5710881f0aafb065c400e632
minzx23 <kmjn0623@gmail.com>
2026-09-07 16:12:22 +0900

    ✨Feat: 홈 화면 콘텐츠 정리

---
+++ src/data/content.ts
@@ -0,0 +1,24 @@
+export const banner = {
+  title: '안전의 기준을 새롭게,\n편리함을 더한 ',
+  titleHighlight: '스마트 도어락',
+  description: '일상의 안전과 편리함을 생각한 디지털 도어락\n',
+  descriptionHighlight: 'ELVIA',
+  description2: '가 새로운 도어락의 기준을 만들어갑니다.',
+};
+
+export const company = {
+  title: '회사 소개',
+  desciption:
+    'DOOR&TECH는 디지털 도어락 전문기업으로 안전성과 편의성을 갖춘 제품을 개발하고 공급합니다. 자체 브랜드 ELVIA(엘비아)를 중심으로 제품 기획, 개발, 품질관리 및 유통에 이르는 체계적인 관리와 고객의 라이프스타일을 반영한 제품을 통해 더 안전하고 편리한 주거환경을 만들어가겠습니다. \nSafety · Convenience · Value',
+};
+
+export const contact = {
+  tel: '',
+  fax: '',
+  email: '',
+  address: '',
+};
+
+export const footer = {
+  copyright: '© 2024 DOOR & TECH CO., LTD. All rights reserved.',
+};
