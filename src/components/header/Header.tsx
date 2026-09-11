1e16a6f628e8682028984a6b4e0bcfdd563ea996
minzx23 <kmjn0623@gmail.com>
2026-09-11 18:36:24 +0900

    📦️Chore: 헤더 레이아웃 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 flex items-center justify-between bg-[#0E2447]'>
+    <header className='sticky top-0 z-50 flex items-center justify-between bg-[#0E2447] md:grid md:grid-cols-[1fr_auto_1fr]'>
       <Link href='/' className='flex items-center gap-2 text-white'>
         <span className='px-8'>
           <Image src='/DNT_logo_white.png' alt='logo' width={120} height={120} />
@@ -14,6 +14,8 @@ export default function Header() {
       <div className='px-8 py-4'>
         <HeaderMenu />
       </div>
+
+      <div className='hidden md:block' />
     </header>
   );
 }
