ec19f0efc15962448652eb7b49b551197c7c548e
minzx23 <kmjn0623@gmail.com>
2026-09-11 18:37:32 +0900

    📦️Chore: 제품 카드 이미지 크기 수정

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -25,13 +25,13 @@ export default function ProductCard({
   return (
     <div>
       {variant === 'home' ? (
-        <div className='flex h-95 w-100 flex-col gap-5 rounded-md border border-gray-300 px-5.25 py-6.25 lg:h-65 lg:w-65 lg:gap-2 lg:py-4'>
+        <div className='flex h-102 w-100 flex-col gap-5 rounded-md px-5.25 py-6.25 lg:h-78 lg:w-75 lg:gap-2 lg:py-4'>
           <Image
             src={image}
             alt='product'
-            width={270}
-            height={168}
-            className='self-center md:w-77 lg:h-auto lg:w-50'
+            width={200}
+            height={200}
+            className='aspect-square h-auto self-center object-cover lg:w-40'
           />
           <div className='flex flex-col gap-2'>
             <p className='text-[16px] font-bold lg:text-[14px]'>{name}</p>
@@ -42,16 +42,16 @@ export default function ProductCard({
           </div>
         </div>
       ) : (
-        <div className='flex flex-col gap-5 rounded-md border border-gray-300 px-5 py-10 md:gap-1 lg:w-120 lg:flex-row'>
+        <div className='flex flex-col items-center gap-5 rounded-md border border-gray-300 px-5 py-10 md:gap-1 lg:w-120 lg:flex-row'>
           <Image
             src={image}
             alt='product'
-            width={270}
-            height={168}
-            className='self-center lg:w-70'
+            width={250}
+            height={250}
+            className='aspect-square self-center object-cover lg:w-60'
           />
           <div className='flex flex-col gap-3'>
-            <p className='text-[16px] font-bold'>{name}</p>
+            <p className='text-[18px] font-bold'>{name}</p>
             <p className='text-[13px]'>{description}</p>
             {price != undefined && (
               <p className='text-[15px] font-bold'>{price.toLocaleString()}원</p>
