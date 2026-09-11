9af24e45268484010c455157ae1d0aab9a494c4c
minzx23 <kmjn0623@gmail.com>
2026-09-11 18:36:53 +0900

    📦️Chore: 홈 화면 하단 로고 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import ProductCard from '@/components/product/ProductCard';
 import { company } from '@/data/content';
 import { products } from '@/data/product';
 import { ChevronRight } from 'lucide-react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -16,7 +15,7 @@ export default function Home() {
         <div className='flex items-center justify-between px-16 py-8 lg:py-4'>
           <h3 className='text-[19px] font-bold md:text-[22px] lg:text-[19px]'>주요 제품</h3>
           <div className='flex items-center gap-1 text-gray-400 hover:cursor-pointer'>
-            <Link href='/products/doorlock' className='text-[12px]'>
+            <Link href='/doorlock' className='text-[12px]'>
               전체보기
             </Link>
             <ChevronRight size={14} />
@@ -40,14 +39,14 @@ export default function Home() {
 
       <hr className='mx-8 mt-6 border-gray-200' />
 
-      <div className='flex px-16 py-8 lg:py-4'>
+      <div className='flex gap-10 px-16 py-8 lg:py-4'>
         <div className='flex min-w-0 flex-1 flex-col gap-1'>
           <h3 className='text-[19px] font-bold md:text-[22px] lg:text-[19px]'>회사 소개</h3>
           <p className='text-[12px]'>{company.desciption}</p>
         </div>
-        <div className='flex min-w-0 flex-1 items-center justify-center'>
+        {/* <div className='flex min-w-0 flex-1 items-center justify-center'>
           <Image src='/ELVIA_logo.png' width={200} height={200} alt='' />
-        </div>
+        </div> */}
         <div className='flex min-w-0 flex-1'>
           <h3 className='text-[19px] font-bold md:text-[22px] lg:text-[19px]'>고객 지원</h3>
         </div>
